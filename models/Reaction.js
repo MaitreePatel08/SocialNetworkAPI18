b59642ef1efb3635a9f343c56b8e05bc088c4ac4
MaitreePatel08 <maitreepatel012@gmail.com>
2023-12-07 16:18:35 -0500

    commit

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const { Schema, Types } = require('mongoose');
+//Importing the function that will be used within a getter to format date strings 
+const formatDate = require('../utils/formatDate');
+//Importing plug-in in order to be able to use the getter when retrieving 'lean' documents
+const mongooseLeanGetters = require('mongoose-lean-getters');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: function () {
+            return formatDate(this.createdAt);
+        }
+    }
+},
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    });
+//Attaching the plug-in to the schema.
+reactionSchema.plugin(mongooseLeanGetters);
+module.exports = reactionSchema;
