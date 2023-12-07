66bbb59ca476d1ddb5b94fd6e7b09b63a62d6dda
MaitreePatel08 <maitreepatel012@gmail.com>
2023-12-07 16:14:57 -0500

    commit

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+//Importing the necesary controllers for the routes
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/userController');
+
+//Routes available for the commented relative paths
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
